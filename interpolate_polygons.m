c11074848541f4b0372b85ff047356d2299f676e
Marcel Beining <marcel.beining@mpimf-heidelberg.mpg.de>
2013-02-15 20:55:21 +0100

    V002: totally different but working

--- interpolate_polygons.m
+++ interpolate_polygons.m
@@ -1,63 +1,85 @@
-contours{1}.Faces = [1 2 3 4 5];
-contours{1}.Vertices = [1 2;5 6;10 12;3 10;2 5];
+contours =[];
+contours{5}.Faces = [1 2 3 4 5];
+contours{5}.Vertices = [2 4;10 12;20 24;6 20;4 10];
 contours{10}.Faces = [1 2 3 4 5];
-contours{10}.Vertices = [1 2;5 6;10 12;3 10;2 5];
+contours{10}.Vertices = [3 10;2 5;1 2;5 6;10 12];
+max_z = 20;
 
-% contours = cell(100,1);
 
 %%
-max_contours = 100;
-curr_contours=0;
-not_empty = ~cellfun(@isempty,contours);
-while curr_contours <= max_contours
-    curr_contours = curr_contours +1;
-    fprintf(sprintf('%d\n',curr_contours));
-%     if curr_contours > numel(contours)
-%         break
-%     end
-    if isempty(contours{curr_contours})
-        pairs =[];
-        this_polygon= [];
-        contour1 = find(not_empty(1:curr_contours),1,'last');
-        contour2 = find(not_empty(curr_contours:end),1,'first')+curr_contours-1;
+contours{max_contours}= [];
+pairs =[];
+
+empty = cellfun(@isempty,contours);
+
+ind = find(~empty,1,'first');
+if isempty(ind)
+    return
+end
+curr_z = ind -1;
+contours(1:curr_z) = contours(ind);
+
+while curr_z <= max_z
+    curr_z = curr_z +1;
+    fprintf(sprintf('%d\n',curr_z));
+    
+    if empty(curr_z)
+        if isempty(pairs)
+            pairs = zeros(max(numel(poly_first.Faces),numel(poly_second.Faces)),2);
+            for p = 1:size(poly_first.Vertices,1)
+                dist = sqrt(sum((repmat(poly_first.Vertices(p,:),[size(poly_second.Vertices,1),1])-poly_second.Vertices).^2,2));
+                [nix mindist] = min(dist);
+                pairs(p,:) = [p,mindist];
+            end
+            remaining_coords = setdiff(1:size(poly_second.Vertices,1),unique(pairs(:,2)));
+            for o = 1:numel(remaining_coords)
+                dist = sqrt(sum((repmat(poly_second.Vertices(remaining_coords(o),:),[size(poly_first.Vertices,1),1])-poly_first.Vertices).^2,2));
+                [nix mindist] = min(dist);
+                pairs(p+o,:) = [mindist,remaining_coords(o)];
+            end
+            pairs = sortrows(pairs);
+            fp = sort(pairs(pairs(:,2) == 1 | pairs(:,2) == poly_second.Faces(end),1));
+            fp = fp(diff(fp)==0);
+            if ~isempty(fp)
+                prob_pairs = pairs(:,1) == fp;
+                partners = pairs(prob_pairs,2);
+                [nix, ind] = max(diff(partners));
+                pairs(prob_pairs,2)=cat(1,partners(ind+1:end),partners(1:ind));
+            end
+            this_polygon.Faces = 1:size(pairs,1);
+        end
         
-        if isempty(contour1) 
-            contour1 = find(not_empty,1,'first');
-            curr_contours = contour1 -1;
-            contours(1:curr_contours) = contours(contour1);
-            not_empty(1:curr_contours) = true;
-            continue
-        elseif isempty(contour2)
-            contours(curr_contours:max_contours) = contours(curr_contours-1);
-            not_empty(curr_contours:max_contours) = true;
+        
+        this_polygon.Vertices = zeros(size(pairs,1),2);
+        for p = 1:size(pairs,1)
+            x = [poly_first.Vertices(pairs(p,1),1),poly_second.Vertices(pairs(p,2),1)];
+            y = [poly_first.Vertices(pairs(p,1),2),poly_second.Vertices(pairs(p,2),2)];
+            this_polygon.Vertices(p,:) = [interp1([z_cont(1),z_cont(2)],x,curr_z),interp1([z_cont(1),z_cont(2)],y,curr_z)];
+        end
+        contours{curr_z} = this_polygon;
+        
+    else
+        poly_first = contours{curr_z};
+        z_cont(1) = curr_z;
+        ind = find(~empty(curr_z+1:end),1,'first');
+        if isempty(ind)
+            contours(curr_z+1:max_z) = {poly_first};
             break
+        else
+            z_cont(2) = ind + curr_z;
+            poly_second = contours{z_cont(2)};
         end
-        
-        poly_first = contours{contour1};
-        poly_second = contours{contour2};
-        
-        for p = 1:sicontourse(poly_first,1)
-            dist = sqrt(sum((repmat(poly_first(p,:),[sicontourse(poly_second,1),1])-poly_second).^2));
-            [nix mindist] = min(dist);
-            pairs(end+1,:) = [p,mindist];
-        end
-        remaining_coords = setdiff([1:sicontourse(poly_second,1)],unique(pairs(:,2)));
-        for p = 1:numel(remaining_coords)
-            dist = sqrt(sum((repmat(poly_second(remaining_coords(p),:),[sicontourse(poly_first,1),1])-poly_first).^2));
-            [nix mindist] = min(dist);
-            pairs(end+1,:) = [mindist,remaining_coords(p)];
-        end
-        
-        for p = 1:sicontourse(pairs,1)
-            x = [poly_first(pairs(p,1),1),poly_second(pairs(p,2),1)];
-            y = [poly_first(pairs(p,1),2),poly_second(pairs(p,2),2)];
-            this_polygon(end+1,:) = [interp1([contour1,contour2],x,curr_contours),interp1([contour1,contour2],y,curr_contours)];
-        end
-        contours{curr_contours} = this_polygon;
-        not_empty(curr_contours) = true;
+        pairs =[];
     end
 end
 
-if curr_contours ~= max_contours
-    errordlg('Möp')
+%%
+figure;hold on
+for i=1:numel(contours)
+    this_patch = contours{i};
+    this_patch.FaceAlpha = 0.3;
+    this_patch.FaceColor = [1 0 1];
+    this_patch.Vertices(:,3)=i;
+   patch(this_patch)
 end
+   
